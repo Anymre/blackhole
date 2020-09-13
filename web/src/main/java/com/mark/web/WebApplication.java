081c3d8427a201ead65c3ac7d43b567a4d25c7e5
mark42 <mengchao.lv@connext.com.cn>
2020-09-13 23:52:05 +0800

    Init

--- web/src/main/java/com/mark/web/WebApplication.java
+++ web/src/main/java/com/mark/web/WebApplication.java
@@ -1,18 +1,27 @@
 package com.mark.web;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication(scanBasePackages = {"com.mark"})
-@EntityScan("com.mark.service.entity")
-@EnableJpaRepositories(basePackages = {"com.mark.service.repository"})
-
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
 
+    @Component
+    class test implements CommandLineRunner {
+        @Value("${a:1}")
+        private int a;
+    
+        @Override
+        public void run(String... args) throws Exception {
+            System.out.println(a);
+        }
+    }
 }
--- web/src/main/java/com/mark/web/controller/WebController.java
+++ web/src/main/java/com/mark/web/controller/WebController.java
@@ -1,8 +1,5 @@
 package com.mark.web.controller;
 
-import com.mark.service.entity.TestEntity;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
