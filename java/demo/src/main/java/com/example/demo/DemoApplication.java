ccd05f95001fd9fd738a05da1def7152a9708fca
mark4z <anymre@outlook.com>
2021-02-02 18:34:23 +0800

    Default Changelist

--- java/demo/src/main/java/com/example/demo/DemoApplication.java
+++ java/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,15 @@
 package com.example.demo;
 
-import io.seata.spring.boot.autoconfigure.SeataAutoConfiguration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude = SeataAutoConfiguration.class)
+@SpringBootApplication
 @EnableDiscoveryClient
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
