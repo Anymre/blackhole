cef736487a422d0aeaeab0e120202f62d2ba0313
mark4z <anymre@outlook.com>
2021-02-05 15:46:28 +0800

    basic done

--- java/demo-B/src/main/java/com/example/demo/Controller.java
+++ java/demo-B/src/main/java/com/example/demo/Controller.java
@@ -3,12 +3,19 @@ package com.example.demo;
 import com.example.demo.entity.TestBEntity;
 import com.example.demo.feign.DemoClient;
 import com.example.demo.repository.TestBRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class Controller {
@@ -18,21 +25,32 @@ public class Controller {
     @Autowired
     private TestBRepository testBRepository;
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
 
     @GetMapping
     @ApiOperation("测试")
-    public String getAll() {
-        return demoClient.getAll();
+    public Object getAll() throws JsonProcessingException {
+        Map<String, Object> map = new HashMap<>(8);
+        map.put("B", testBRepository.findAll());
+        map.put("A", demoClient.getAll());
+        return map;
     }
 
     @PostMapping
     @ApiOperation("shift")
-    public String shift(@RequestParam int v) {
+    @Transactional(rollbackFor = Exception.class)
+    public String shift(@RequestParam int v) throws Exception {
+        demoClient.incr(-v);
+
+
         Iterable<TestBEntity> all = testBRepository.findAll();
         all.forEach(i -> i.setValue(i.getValue() + v));
         testBRepository.saveAll(all);
 
-        demoClient.incr(-v);
+        if (v % 2 == 1) {
+            throw new Exception("" + v);
+        }
 
         return demoClient.getAll();
     }
